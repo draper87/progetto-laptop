580db4223db2f17e618c8b7822cbd6fdef4d3b9b
Oliver Benoit <oliver.benoit@triboo.it>
2021-04-02 23:55:09 +0200

    Bugfix wishlist.js

--- public/js/wishlist.js
+++ public/js/wishlist.js
@@ -1,15 +1,26 @@
 $(document).ready(function () {
 
+    // dichiaro variabili
     let array_laptops = [];
     let array_localStorage = localStorage.getItem('wishlist');
     let array_wishlist_not_parsed_int;
     let array_wishlist;
     if (array_localStorage) {
+        // trasformo il mio localStorage in un array dato che essendo uno storage in realtà è una stringa di numeri divisi da virgole.
+        // tolgo il primo carattere della stringa '['
         array_localStorage = array_localStorage.substring(1);
+
+        // tolgo l ultimo carattere della stringa ']'
         array_localStorage = array_localStorage.substring(0, array_localStorage.length - 1);
+
+        // trasformo la stringa in array
         array_wishlist_not_parsed_int = array_localStorage.split(',')
+
+        // trasformo tutti gli elementi dell array in integer per evitare incongruenze di dati.
         array_wishlist = array_wishlist_not_parsed_int.map((x) => parseInt(x));
     }
+
+    // localStorage.clear();
 
     let laptopId;
     $(document).on('click', '.wishlist-add-button',
@@ -19,11 +30,10 @@ $(document).ready(function () {
             if (!array_wishlist && !array_laptops.includes(laptopId)) {
                 array_laptops.push(laptopId);
                 localStorage.setItem('wishlist', JSON.stringify(array_laptops));
-                console.log('setto')
+                // console.log('setto')
                 $(this).toggleClass('opaque');
             } else if (!array_wishlist && array_laptops.includes(laptopId)) {
-                // let laptop_index = array_laptops.indexOf(laptopId)
-                // // array_laptops = array_laptops.splice(laptop_index, 1)
+                // elimino l elemento dall array che corrisponde a laptopId
                 for (let i = 0; i < array_laptops.length; i++) {
                     if (array_laptops[i] === laptopId)
                     array_laptops.splice(i, 1);
@@ -31,12 +41,12 @@ $(document).ready(function () {
                 localStorage.setItem('wishlist', JSON.stringify(array_laptops));
                 $(this).toggleClass('opaque');
             } else if (array_wishlist && !array_wishlist.includes(laptopId)) {
-                array_laptops.push(laptopId);
-                let concat_array = array_wishlist.concat(array_laptops);
-                console.log(concat_array)
-                localStorage.setItem('wishlist', JSON.stringify(concat_array));
+                array_wishlist.push(laptopId);
+                // console.log(array_wishlist)
+                localStorage.setItem('wishlist', JSON.stringify(array_wishlist));
                 $(this).toggleClass('opaque');
             } else {
+                // elimino l elemento dall array che corrisponde a laptopId
                 for (let i = 0; i < array_wishlist.length; i++) {
                     if (array_wishlist[i] === laptopId)
                         array_wishlist.splice(i, 1);
